a59f1dca54393c8d9e21489d935ab5f48cad3549
rdanger2991 <rdanger2991@gmail.com>
2025-03-08 11:18:54 -0500

    week1-assignment

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,71 @@
 using System;
 
-class Program
+namespace Exercise2
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Enter your grade percentage: ");
+            string userInput = Console.ReadLine();
+
+            int percentage = int.Parse(userInput);
+
+                string letter = "";
+
+                if (percentage >= 90)
+                {
+                    letter = "A";
+                }
+                else if (percentage >= 80)
+                {
+                    letter = "B";
+                }
+                else if (percentage >= 70)
+                {
+                    letter = "C";
+                }
+                else if (percentage >= 60)
+                {
+                    letter = "D";
+                }
+                else
+                {
+                    letter = "F";
+                }
+
+                string sign = "";
+                int lastDigit = percentage % 10;
+
+                if (letter != "F") 
+                {
+                    if (lastDigit >= 7)
+                    {
+                        sign = "+";
+                    }
+                    else if (lastDigit < 3)
+                    {
+                        sign = "-";
+                    }
+                }
+
+                if (letter == "A" && sign == "+")
+                {
+                    sign = ""; 
+                }
+
+                Console.WriteLine($"Your grade is: {letter}{sign}");
+
+                if (percentage >= 70)
+                {
+                    Console.WriteLine("Congratulations! You passed the course.");
+                }
+                else
+                {
+                    Console.WriteLine("You did not pass the course. Keep working hard for next time!");
+                }
+            }
+          
+        }
     }
-}
--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        bool playAgain = true;
+
+        while (playAgain)
+        {
+            Random random = new Random();
+            int magicNumber = random.Next(1, 101);
+
+            int guess = 0;
+            bool isGuessed = false;
+            int guessCount = 0;
+
+            while (!isGuessed)
+            {
+
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                guessCount++;
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"It took you {guessCount} guesses.");
+                    isGuessed = true;
+                }
+            }
+
+            Console.Write("Do you want to play again? (yes/no): ");
+            string playAgainResponse = Console.ReadLine().ToLower();
+
+            if (playAgainResponse != "yes")
+            {
+                playAgain = false;
+            }
+        }
+
+        Console.WriteLine("Thanks for playing!");
     }
 }
